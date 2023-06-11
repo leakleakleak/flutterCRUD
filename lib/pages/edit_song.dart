6728f858d9067251f2b563498f32975970cd6370
Karl Diolanda <diolanda.karlmichaelpup@gmail.com>
2023-06-11 13:02:31 +0800

    CRUD feature complete

--- lib/pages/edit_song.dart
+++ lib/pages/edit_song.dart
@@ -1,3 +1,5 @@
+import 'dart:html';
+
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:date_field/date_field.dart';
 import 'package:flutter/material.dart';
@@ -6,22 +8,38 @@ import 'package:intl/intl.dart';
 
 import '../model/user.dart';
 
-class AddSongPage extends StatefulWidget {
-  const AddSongPage({super.key});
+class EditSongPage extends StatefulWidget {
+  final String songID;
+  final String songName;
+  final String albumName;
+  final DateTime releaseDate;
+  const EditSongPage(
+      {super.key,
+      required this.songName,
+      required this.albumName,
+      required this.releaseDate,
+      required this.songID});
 
   @override
-  State<AddSongPage> createState() => _AddSongPage();
+  State<EditSongPage> createState() => _EditSongPage();
 }
 
-class _AddSongPage extends State<AddSongPage> {
+class _EditSongPage extends State<EditSongPage> {
   final formKey = GlobalKey<FormState>();
-  final controllerSongName = TextEditingController();
-  final controllerSongAlbum = TextEditingController();
-
-  DateTime? dateTime;
-  String _name = '';
+  late TextEditingController controllerSongName;
+  late TextEditingController controllerSongAlbum;
+  late DateTime? dateTime;
+  late String songID;
 
   @override
+  void initState() {
+    super.initState();
+    controllerSongName = TextEditingController(text: widget.songName);
+    controllerSongAlbum = TextEditingController(text: widget.albumName);
+    dateTime = widget.releaseDate;
+    songID = widget.songID;
+  }
+
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
@@ -34,7 +52,6 @@ class _AddSongPage extends State<AddSongPage> {
             key: formKey,
             child: Column(
               children: [
-                //-----------------------SONG NAME
                 TextFormField(
                   controller: controllerSongName,
                   validator: (value) {
@@ -43,16 +60,10 @@ class _AddSongPage extends State<AddSongPage> {
                     }
                     return null;
                   },
-                  onSaved: (value) {
-                    _name = value ?? '';
-                  },
                   decoration: const InputDecoration(
-                    labelText: 'Song Name',
-                    border: OutlineInputBorder(),
-                  ),
+                      labelText: 'Song Name', border: OutlineInputBorder()),
                 ),
                 const SizedBox(height: 24),
-                //-----------------------ALBUM NAME
                 TextFormField(
                   controller: controllerSongAlbum,
                   validator: (value) {
@@ -62,13 +73,12 @@ class _AddSongPage extends State<AddSongPage> {
                     return null;
                   },
                   decoration: const InputDecoration(
-                    labelText: 'Album Name',
-                    border: OutlineInputBorder(),
-                  ),
+                      labelText: 'Album', border: OutlineInputBorder()),
                 ),
-                //-----------------------RELEASE DATE
                 const SizedBox(height: 24),
                 DateTimeFormField(
+                  initialDate: dateTime,
+                  initialValue: dateTime,
                   mode: DateTimeFieldPickerMode.date,
                   dateFormat: DateFormat.yMd(),
                   validator: (value) {
@@ -92,25 +102,26 @@ class _AddSongPage extends State<AddSongPage> {
                 SizedBox(
                   height: 35,
                   child: ElevatedButton(
-                    child: const Text('Create'),
+                    child: const Text('Update'),
                     onPressed: () {
                       if (formKey.currentState!.validate()) {
                         formKey.currentState!.save();
                         DateTime selectedDate = dateTime!;
-                        final user = User(
-                            songName: controllerSongName.text,
-                            albumName: controllerSongAlbum.text,
-                            releaseDate: selectedDate);
-                        createUser(user);
+                        if (selectedDate != null) {
+                          final user = User(
+                              songName: controllerSongName.text,
+                              albumName: controllerSongAlbum.text,
+                              releaseDate: selectedDate);
+                          editUser(user, songID);
+                        }
                         Navigator.pop(context);
                         Fluttertoast.showToast(
-                            msg: 'Added Succesfully!',
+                            msg: 'Edited Succesfully',
                             toastLength: Toast.LENGTH_SHORT,
                             gravity: ToastGravity.BOTTOM,
                             webPosition: 'center',
                             backgroundColor: Colors.blue);
                       }
-                      //converting the null aware data type to a standard data type
                     },
                   ),
                 ),
@@ -122,9 +133,9 @@ class _AddSongPage extends State<AddSongPage> {
     );
   }
 
-  Future createUser(User user) async {
+  Future editUser(User user, songID) async {
     //instantiate new collection 'user'
-    final docUser = FirebaseFirestore.instance.collection('songs').doc();
+    final docUser = FirebaseFirestore.instance.collection('songs').doc(songID);
     //get the instantiated collection id and pass the value to user.id
     user.id = docUser.id;
     //convert to json
--- lib/pages/landing_page.dart
+++ lib/pages/landing_page.dart
@@ -1,9 +1,12 @@
+import 'dart:html';
+
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:fluttertoast/fluttertoast.dart';
 import 'package:intl/intl.dart';
 import 'package:todolist_crud/model/user.dart';
-
-import 'add_song.dart';
+import 'package:todolist_crud/pages/add_song.dart';
+import 'package:todolist_crud/pages/edit_song.dart';
 
 class LandingPage extends StatelessWidget {
   const LandingPage({super.key});
@@ -12,24 +15,69 @@ class LandingPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text('Carly Rae Jepsen Songs')),
-      body: StreamBuilder<List<User>>(
-        stream: readUsers(),
+      body: StreamBuilder<QuerySnapshot>(
+        stream: FirebaseFirestore.instance.collection('songs').snapshots(),
         builder: (context, snapshot) {
-          if (snapshot.hasData) {
-            final users = snapshot.data!;
-            return ListView(
-              children: users.map(buildUser).toList(),
-            );
-          } else {
-            return const Center(
-              child: CircularProgressIndicator(),
-            );
+          if (snapshot.hasError) {
+            return Text('Error: ${snapshot.error}');
           }
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return Center(child: CircularProgressIndicator());
+          }
+
+          return ListView(
+            children: snapshot.data!.docs.map((DocumentSnapshot document) {
+              Map<String, dynamic> data =
+                  document.data() as Map<String, dynamic>;
+              String title = '${data['albumName']} || ${data['songName']}';
+              Timestamp subtitle = data['releaseDate'];
+
+              return ListTile(
+                leading: CircleAvatar(child: Text('#')),
+                title: Text(title),
+                subtitle: Text(DateFormat('yMd').format(subtitle.toDate())),
+                trailing: Row(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    IconButton(
+                      icon: Icon(Icons.edit),
+                      onPressed: () {
+                        showDialog(
+                          context: context,
+                          builder: (BuildContext context) {
+                            return EditSongPage(
+                              songName: data['songName'],
+                              albumName: data['albumName'],
+                              releaseDate: subtitle.toDate(),
+                              songID: data['id'],
+                            );
+                          },
+                        );
+                      },
+                    ),
+                    IconButton(
+                      icon: Icon(Icons.delete),
+                      onPressed: () {
+                        deleteSong(data['id']);
+                        Fluttertoast.showToast(
+                            msg:
+                                'The Song ${data['songName']} is succesfully Deleted!',
+                            toastLength: Toast.LENGTH_SHORT,
+                            gravity: ToastGravity.BOTTOM,
+                            webPosition: 'center',
+                            backgroundColor: Colors.blue);
+                      },
+                    ),
+                  ],
+                ),
+              );
+            }).toList(),
+          );
         },
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
-          showModalBottomSheet(
+          showDialog(
               context: context,
               builder: (BuildContext context) {
                 return const AddSongPage();
@@ -40,15 +88,16 @@ class LandingPage extends StatelessWidget {
     );
   }
 
-  Widget buildUser(User user) => ListTile(
-        leading: Text(
-          'Album: ${user.albumName}',
-        ),
-        title: Text(user.songName),
-        subtitle:
-            Text('Release Date: ${DateFormat('yMd').format(user.releaseDate)}'),
-      );
+  deleteSong(songID) async {
+    //instantiate new collection 'user'
+    final docUser = FirebaseFirestore.instance.collection('songs').doc(songID);
+    //get the instantiated collection id and pass the value to user.id
 
+    //submit to firebase
+    await docUser.delete();
+  }
+
+  //get data from firebase
   Stream<List<User>> readUsers() => FirebaseFirestore.instance
       .collection('songs')
       .snapshots()
